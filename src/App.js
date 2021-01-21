cbfcf1c91e4e9ee3594a0603fe4fcffdfe27ee78
koderoom <koderoom.dev@gmail.com>
2021-01-21 12:36:28 +0530

    lifecycle.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div>Hello World</div>;
+  const [title, setTitle] = useState("My Project");
+  const [list, setList] = useState([]);
+
+  // Will be called, when our compoent is getting loaded.
+  // How many times is going to called :: ideally 1
+  useEffect(() => {
+    console.log("I am called on the load of the component.");
+
+    setTitle("My Project Book");
+
+    let tempList = ["Delhi", "Calcutta", "Mumbai", "Chennai"];
+    setList(tempList);
+  }, []);
+
+  return (
+    <div>
+      Hello World
+      <div>{title}</div>
+      {list.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
