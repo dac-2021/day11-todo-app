be1470d61ecbc1c733ac24ac919baf3775503783
koderoom <koderoom.dev@gmail.com>
2021-01-21 21:53:04 +0530

    t

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, useLocation, useHistory } from "react-router-dom";
+import { Route, useLocation, useHistory, Link } from "react-router-dom";
 
 function App() {
   let location = useLocation();
@@ -7,13 +7,30 @@ function App() {
 
   return (
     <>
+      {location.pathname !== "/login" && <Navbar />}
       <Route exact path="/" component={Page1} />
-      <Route path="/page1" component={Page1} />
+      <Route path="/login" component={Login} />
 
+      <Route path="/page1" component={Page1} />
       <Route path="/page2" component={Page2} />
       <Route path="/page3" component={Page3} />
     </>
   );
+}
+
+function Navbar() {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/page1">Page1</Link>
+      <Link to="/page2">Page2</Link>
+      <Link to="/page3">Page3</Link>
+    </>
+  );
+}
+
+function Login() {
+  return <div>Login</div>;
 }
 
 function Page1() {
