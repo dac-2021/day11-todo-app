b4c50870f740d74d7b7e0613f9396ea6dbb3a71e
koderoom <koderoom.dev@gmail.com>
2021-01-21 10:28:35 +0530

    changes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="bg-primary">Hello Universe</div>;
+  return <Todo />;
+}
+
+function Todo() {
+  const [taskList, setTaskList] = useState([
+    { id: 1, task: "Task1", complete: false },
+    { id: 2, task: "Task2", complete: true },
+    { id: 3, task: "Task3", complete: true },
+    { id: 4, task: "Task4", complete: false },
+  ]);
+
+  const processTask = () => {};
+
+  return (
+    <div>
+      <h3>Todo Application</h3>
+
+      {/** Input Block */}
+      <div>
+        <input type="text" placeholder="Add your task here" />
+        <button>Add Task</button>
+      </div>
+
+      {/** New/Incomplete Task List */}
+      {taskList.map((item, index) => (
+        <div key={index}>
+          <input
+            type="checkbox"
+            checked={item.complete}
+            onChange={processTask}
+          />
+          <span style={{ marginLeft: "4px" }}>{item.task}</span>
+          <button style={{ marginLeft: "16px" }}>DEL</button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
